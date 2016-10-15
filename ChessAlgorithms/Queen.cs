aa2fcfb5d70fcde128cb5cb8f7e1f7956bef6c7f
Łukasz Rabiec <lukaszrabiec@gmail.com>
2016-10-15 23:48:39 +0200

    Queen init

---
+++ ChessAlgorithms/Queen.cs
@@ -0,0 +1,12 @@
+﻿namespace ChessAlgorithms
+{
+    // Codemian Rhapsody:
+    //    Is this the real code?
+    //    Is this just fantasy?
+    //    Caught during pushing, no escape from git history.
+    //    Open your IDE, look up to this code and see...
+    public class Queen
+    {
+
+    }
+}
